6976ec1448efcc9471447330a356007d433c806d
Adam Cigánek <adam.ciganek@gmail.com>
2016-04-28 12:03:37 +0200

    Add support for libsodium cross-compilation

--- Bash Scripts/Travis/install_libsodium.sh
+++ Bash Scripts/Travis/install_libsodium.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
+set -ex
+
 # Set the libsodium version if it isn't already set
 if [ -z "$LibSodiumVersion" ]; then
   LibSodiumVersion=1.0.9
 fi
 
+case $TARGET; in
+  arm*-gnueabihf)
+    HOST="--host=arm-linux-gnueabihf"
+    ;;
+  x86_64-unknown-linux-musl)
+    HOST="--host=x86_64-linux-musl"
+    ;;
+esac
+
 # Check to see if libsodium dir has been retrieved from cache
 LibSodiumInstallPath=$HOME/libsodium/$LibSodiumVersion
 if [ ! -d "$LibSodiumInstallPath/lib" ]; then
   # If not, build and install it
-  cd $HOME
+  pushd $HOME
   rm -rf libsodium
   mkdir -p temp
   cd temp
   wget https://github.com/jedisct1/libsodium/releases/download/$LibSodiumVersion/libsodium-$LibSodiumVersion.tar.gz
   tar xfz libsodium-$LibSodiumVersion.tar.gz
   cd libsodium-$LibSodiumVersion
-  ./configure --prefix=$LibSodiumInstallPath --enable-shared=no --disable-pie
+  ./configure $HOST --prefix=$LibSodiumInstallPath --enable-shared=no --disable-pie
   Cores=$((hash nproc 2>/dev/null && nproc) || (hash sysctl 2>/dev/null && sysctl -n hw.ncpu) || echo 1)
-  make check -j$Cores
+  make -j$Cores
   make install
+  popd
 else
   echo "Using cached libsodium directory (version $LibSodiumVersion)";
 fi
